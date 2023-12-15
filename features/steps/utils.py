2c294d0c6f9d6761c0ca3815fe6bf7d9fe7d4802
Andreas Neuber <github@andy-neuber.de>
2023-12-15 13:37:18 +0100

    Add UserProvider class

--- features/steps/utils.py
+++ features/steps/utils.py
@@ -1,1 +1,17 @@
 # Some important util things go in here, usually functionality used in several step classes...
+
+class UserProvider(object):
+
+    def __init__(self):
+        self.sth = "sth"
+
+    def get_user(self, options):
+
+        # Based on options get appropriate user from data source
+
+        user = {
+            "username": "test1",
+            "password": "1234",
+            "email": "test1@example.org",
+        }
+        return user
